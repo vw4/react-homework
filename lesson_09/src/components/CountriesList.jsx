3d45aca8f62c0f69d8b6bf9652be0ffdffaf8252
vw4 <vyvn@ukr.net>
2023-09-23 16:40:03 +0300

    lesson 09 - delete country implementation

--- lesson_09/src/components/CountriesList.jsx
+++ lesson_09/src/components/CountriesList.jsx
@@ -1,12 +1,19 @@
 import {useCountryData} from "../hooks/useCountryData";
 import {Button, Card, ListGroup} from "react-bootstrap";
+import _ from "lodash";
 
 export default function CountriesList() {
-    const {countryData} = useCountryData();
+    const {countryData, setCountryData} = useCountryData();
 
     if (!countryData) {
         return;
     }
+
+    const onCountryDeleteClick = (e) => {
+        const {country} = e.target.dataset;
+        setCountryData(_.reject(countryData, {name: {official: country}}));
+    }
+
     const CountryListItem = ({flag, name}) => {
         return <ListGroup.Item
             className="d-flex justify-content-between align-items-start"
@@ -14,14 +21,15 @@ export default function CountriesList() {
             <div className="ms-2 me-auto">
                 {flag} <a href={`/country/${name}`}>{name}</a>
             </div>
-            <Button size="sm" onClick={() => alert('Not implemented yet!')}>Delete</Button>
+            <Button size="sm" onClick={onCountryDeleteClick} data-country={name}>Delete</Button>
         </ListGroup.Item>
     }
+
     return <Card>
         <Card.Header>Countries List</Card.Header>
         <Card.Body>
             <ListGroup>
-                {countryData.map(country => <CountryListItem flag={country.flag} name={country.name.official}/>)}
+                {countryData.map(country => <CountryListItem key={country.name.official} flag={country.flag} name={country.name.official}/>)}
             </ListGroup>
         </Card.Body>
     </Card>;
